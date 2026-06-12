af45f2bc849a6622defd1baaf3b86347091666fb
Kaustubh <kaustubhnikam299@gmail.com>
2026-06-13 01:46:00 +0530

    cors error fix

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://your-vercel-app.vercel.app'],
+    origin: ['http://localhost:5173', 'https://techbazaar-kappa.vercel.app/x'],
     credentials: true
 }))
 
